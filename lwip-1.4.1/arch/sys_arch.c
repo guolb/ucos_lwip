480b8df5630b690bf7db536af4174a395521971b
guolb <guolb57@163.com>
2018-01-28 00:13:36 +0800

    implement lwip system arch

--- lwip-1.4.1/arch/sys_arch.c
+++ lwip-1.4.1/arch/sys_arch.c
@@ -14,14 +14,29 @@ OS_STK  TCPIP_THREAD_TASK_STK[TCPIP_THREAD_STACKSIZE];//内核任务堆栈
 //         其他,创建失败
 err_t sys_mbox_new( sys_mbox_t *mbox, int size)
 {
-		//消息队列创建错误 
+	(*mbox)=mymalloc(sizeof(LWip_MBOX));	//为消息邮箱申请内存
+	memset((*mbox),0,sizeof(LWip_MBOX));    //清除mbox的内存
+	if(*mbox)//内存分配成功
+	{
+		if(size>MBOX_MAX_SIZE)size=MBOX_MAX_SIZE;		         //消息队列最多容纳MAX_QUEUE_ENTRIES消息数目 
+ 		(*mbox)->pst=OSQCreate(&((*mbox)->pvQEntries[0]),size);  //使用UCOS创建一个消息队列
+		if((*mbox)->pst!=NULL)return ERR_OK;                     //返回ERR_OK,表示消息队列创建成功 ERR_OK=0
+		else
+		{ 
+			myfree((*mbox));
+			return ERR_MEM;  		//消息队列创建错误
+		}
+	}else return ERR_MEM; 			//消息队列创建错误 
 } 
 
 //释放并删除一个消息邮箱
 //*mbox:要删除的消息邮箱
 void sys_mbox_free(sys_mbox_t * mbox)
 {
-
+	u8_t ucErr;
+	sys_mbox_t m_box=*mbox;   
+	OSQDel(m_box->pst,OS_DEL_ALWAYS,&ucErr);
+	(*mbox)=myfree(*mbox);
 }
 
 //向消息邮箱中发送一条消息(阻塞进程直到发送成功)
@@ -29,7 +44,7 @@ void sys_mbox_free(sys_mbox_t * mbox)
 //*msg:要发送的消息
 void sys_mbox_post(sys_mbox_t *mbox,void *msg)
 {     
-
+	while(OSQPost((*mbox)->pst,msg)!=OS_ERR_NONE);//死循环等待消息发送成功 
 }
 
 //向一个消息邮箱发送消息(发送一次，成功与否都不阻塞进程)
@@ -39,7 +54,8 @@ void sys_mbox_post(sys_mbox_t *mbox,void *msg)
 // 	     ERR_MEM,发送失败
 err_t sys_mbox_trypost(sys_mbox_t *mbox, void *msg)
 { 
-
+	if((OSQPost((*mbox)->pst, msg))!=OS_ERR_NONE)return ERR_MEM;
+	return ERR_OK;
 }
 //等待邮箱中的消息
 //*mbox:消息邮箱
@@ -49,7 +65,30 @@ err_t sys_mbox_trypost(sys_mbox_t *mbox, void *msg)
 //		失败的话就返回超时SYS_ARCH_TIMEOUT
 u32_t sys_arch_mbox_fetch(sys_mbox_t *mbox, void **msg, u32_t timeout)
 { 
-
+	u8_t ucErr;
+	u32_t ucos_timeout,timeout_new;
+	void *temp;
+	sys_mbox_t m_box=*mbox;
+	if(timeout!=0)
+	{
+		ucos_timeout=(timeout*OS_TICKS_PER_SEC)/1000; //转换为节拍数,因为UCOS延时使用的是节拍数,而LWIP是用ms
+		if(ucos_timeout<1)ucos_timeout=1;//至少1个节拍
+	}else ucos_timeout = 0; 
+	timeout = OSTimeGet(); //获取系统时间 
+	temp=OSQPend(m_box->pst,(u16_t)ucos_timeout,&ucErr); //请求消息队列,等待时限为ucos_timeout
+	if(msg!=NULL)
+	{	
+		*msg=temp;									    
+	}    
+	if(ucErr==OS_ERR_TIMEOUT)timeout=SYS_ARCH_TIMEOUT;  //请求超时
+	else
+	{ 
+		timeout_new=OSTimeGet();
+		if (timeout_new>timeout) timeout_new = timeout_new - timeout;//算出请求消息或使用的时间
+		else timeout_new = 0xffffffff - timeout + timeout_new; 
+		timeout=timeout_new*1000/OS_TICKS_PER_SEC + 1; 
+	}
+	return timeout; 
 }
 //尝试获取消息，不阻塞进程
 //*mbox:消息邮箱
@@ -57,7 +96,7 @@ u32_t sys_arch_mbox_fetch(sys_mbox_t *mbox, void **msg, u32_t timeout)
 //返回值:等待消息所用的时间/SYS_ARCH_TIMEOUT
 u32_t sys_arch_mbox_tryfetch(sys_mbox_t *mbox, void **msg)
 {
-
+	return sys_arch_mbox_fetch(mbox,msg,1);//尝试获取一个消息
 }
 
 //检查一个消息邮箱是否有效
@@ -66,14 +105,21 @@ u32_t sys_arch_mbox_tryfetch(sys_mbox_t *mbox, void **msg)
 //      0,无效
 int sys_mbox_valid(sys_mbox_t *mbox)
 {  
-
+	sys_mbox_t m_box=*mbox;
+	u8_t ucErr;
+	int ret;
+	OS_Q_DATA q_data;
+	memset(&q_data,0,sizeof(OS_Q_DATA));
+	ucErr=OSQQuery (m_box->pst,&q_data);
+	ret=(ucErr<2&&(q_data.OSNMsgs<q_data.OSQSize))?1:0;
+	return ret;
 } 
 
 //设置一个消息邮箱为无效
 //*mbox:消息邮箱
 void sys_mbox_set_invalid(sys_mbox_t *mbox)
 {
-
+	*mbox=(void *)0;
 }
 
 //创建一个信号量
@@ -83,7 +129,9 @@ void sys_mbox_set_invalid(sys_mbox_t *mbox)
 // 	     ERR_MEM,创建失败
 err_t sys_sem_new(sys_sem_t * sem, u8_t count)
 {  
-
+	*sem=OSSemCreate((u16_t)count);
+	if(*sem==NULL)return ERR_MEM; 
+	return ERR_OK;
 } 
 
 //等待一个信号量
@@ -93,21 +141,42 @@ err_t sys_sem_new(sys_sem_t * sem, u8_t count)
 //		失败的话就返回超时SYS_ARCH_TIMEOUT
 u32_t sys_arch_sem_wait(sys_sem_t *sem, u32_t timeout)
 { 
-
+	u8_t ucErr;
+	u32_t ucos_timeout, timeout_new; 
+	if(	timeout!=0) 
+	{
+		ucos_timeout = (timeout * OS_TICKS_PER_SEC) / 1000;//转换为节拍数,因为UCOS延时使用的是节拍数,而LWIP是用ms
+		if(ucos_timeout < 1)
+		ucos_timeout = 1;
+	}else ucos_timeout = 0; 
+	timeout = OSTimeGet();  
+	OSSemPend (*sem,(u16_t)ucos_timeout, (u8_t *)&ucErr);
+ 	if(ucErr == OS_ERR_TIMEOUT)timeout=SYS_ARCH_TIMEOUT;//请求超时	
+	else
+	{     
+ 		timeout_new = OSTimeGet(); 
+		if (timeout_new>=timeout) timeout_new = timeout_new - timeout;
+		else timeout_new = 0xffffffff - timeout + timeout_new;
+ 		timeout = (timeout_new*1000/OS_TICKS_PER_SEC + 1);//算出请求消息或使用的时间(ms)
+	}
+	return timeout;
 }
 
 //发送一个信号量
 //sem:信号量指针
 void sys_sem_signal(sys_sem_t *sem)
 {
-
+	OSSemPost(*sem);//调用UCOS下OSSemPost函数
 }
 
 //释放并删除一个信号量
 //sem:信号量指针
 void sys_sem_free(sys_sem_t *sem)
 {
-
+	u8_t ucErr;//定义一个变量，存放错误代码
+	OSSemDel(*sem,OS_DEL_ALWAYS,&ucErr );//调用UCOS下OSSemDel函数，删除一个信号量
+	if(ucErr!=OS_ERR_NONE)LWIP_ASSERT("OSSemDel ",ucErr==OS_ERR_NONE);
+	*sem = (void *)0;
 } 
 
 //查询一个信号量的状态,无效或有效
@@ -116,20 +185,21 @@ void sys_sem_free(sys_sem_t *sem)
 //      0,无效
 int sys_sem_valid(sys_sem_t *sem)
 {
-
+ 	OS_SEM_DATA  sem_data;                                       //定义一个UCOS信号量结构
+	return (OSSemQuery (*sem,&sem_data) == OS_ERR_NONE )? 1:0;   //调用UCOS函数 OSSemQuery，查询信号量状态 
 } 
 
 //设置一个信号量无效
 //sem:信号量指针
 void sys_sem_set_invalid(sys_sem_t *sem)
 {
-
+	*sem=(void *)0; //指向空指针
 } 
 
 //arch初始化
 void sys_init(void)
 { 
-
+    //这里,我们在该函数,不做任何事情
 } 
 
 
@@ -141,12 +211,22 @@ void sys_init(void)
 //prio:进程任务的优先级
 sys_thread_t sys_thread_new(const char *name, lwip_thread_fn thread, void *arg, int stacksize, int prio)
 {
-
+	OS_CPU_SR cpu_sr;
+	if(strcmp(name,TCPIP_THREAD_NAME)==0)//创建TCP IP内核任务
+	{
+		OS_ENTER_CRITICAL();  //进入临界区 
+		OSTaskCreate(thread,arg,(OS_STK*)&TCPIP_THREAD_TASK_STK[stacksize-1],prio);//创建TCP IP内核任务 
+		OS_EXIT_CRITICAL();  //退出临界区
+	} 
+	return 0;
 } 
 
 //获取系统时间
 //返回值:当前系统时间(单位:毫秒)
 u32_t sys_now(void)
 {
-		                      //返回lwip_time;
+	u32_t ucos_time, lwip_time;
+	ucos_time=OSTimeGet();	                      //获取当前系统时间 得到的是UCSO的节拍数
+	lwip_time=(ucos_time*1000/OS_TICKS_PER_SEC+1);//将节拍数转换为LWIP的时间MS
+	return lwip_time; 		                      //返回lwip_time;
 }
